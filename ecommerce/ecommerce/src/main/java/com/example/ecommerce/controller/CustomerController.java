560aad67a3c7977858335cb0155be36fda1ad072
Thalath04 <mohamedthalath407@gmail.com>
2023-10-15 19:01:02 +0530

    boot project

---
+++ ecommerce/ecommerce/src/main/java/com/example/ecommerce/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/customer")
+public class CustomerController {
+	@GetMapping
+	public void lo
+
+}
